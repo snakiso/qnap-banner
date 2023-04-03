53eb8838040452d266d7782f3c051027fec55032
igorchagov <igorchagov@mail.ru>
2023-04-03 18:43:16 +0300

    add swiper

---
+++ src/js/slider.js
@@ -0,0 +1,12 @@
+const swiper4 = new Swiper('.promotion-slider', {
+ slidesPerView: 'auto',
+ autoWidth: true,
+ centeredSlides: true,
+ spaceBetween: 100,
+ loop: true,
+ 768: {
+  slidesPerView: 'auto',
+  freeMode: true,
+  centeredSlides: true,
+ }
+});
